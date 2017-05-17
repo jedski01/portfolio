0d89bb63458dec0b884fe06b82810c8ba7d051fe
Jed Iquin <jedikin@gmail.com>
2017-05-16 22:54:32 -0700

    initial commit

---
+++ script/index.js
@@ -0,0 +1,22 @@
+var captions = [
+    'Login page',
+    'Registration page',
+    'Room creation page',
+    'Chat room page',
+    'Main window with pie chart of TCP UDP Distribution',
+    'Main window with bar chart of TCP UDP Distribution',
+    'Ethernet Conversation',
+    'IPv4 Conversation',
+    'TCP Conversation',
+    'UDP Conversation',
+]
+function showModal(id) {
+    var modal = document.getElementById('myModal');
+    var image = document.getElementById('modalImage');
+    var caption = document.getElementById('caption');
+    
+    image.src = "/images/"+id+".PNG";
+    caption.innerHTML = captions[id];
+    
+    modal.style.display = "block";
+}
